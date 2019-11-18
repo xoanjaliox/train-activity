dc520de800e1d09db7e00c6e44e568b906f3d178
Anjali Sharma <anjalishar89@gmail.com>
2019-11-17 20:48:36 -0800

    Adds updated html, css and js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,10 +19,10 @@ firebase.initializeApp(firebaseConfig);
 var database= firebase.database();
 
 //set to empty string
-var trainName="";
-var destination="";
-var firstTrainTime="";
-var frequencyMin="";
+var trainName=" ";
+var destination=" ";
+var firstTrainTime=" ";
+var frequencyMin=" ";
 
 //current time for display and later use 
 var time= moment().format('LT')
@@ -68,43 +68,43 @@ var dest= childSnapshot.val().destination;
 var trainTime= childSnapshot.val().firstTrainTime;
 var frequency= childSnapshot.val().frequencyMin;
 
-console.log("train name" + " "+ train);
-console.log("train destination" + " "+ dest);
-console.log("train time" + " "+ trainTime);
-console.log("train frequency" + " "+ frequency);
+console.log("Train name: " + " " + train);
+console.log("Train destination: " + " " + dest);
+console.log("Train time: " + " " + trainTime);
+console.log("Train frequency: " + " " + frequency);
 
 //change time so it's like the train already came
-var takeMeBack= moment(trainTime, "hh:mm").subtract(1,"years");
+var takeBackTime= moment(trainTime, "hh:mm").subtract(1,"years");
 
 
-console.log ("tmb"+ " "+takeMeBack);
+console.log ("Take back time: " + " " + takeBackTime);
 
  //putting in minutes
-var difference= moment().diff(moment(takeMeBack), "minutes")
+var difference= moment().diff(moment(takeBackTime), "minutes")
 
-console.log("difference" +" " +difference);
+console.log("Difference: " + " " +difference);
 
 //divide by frequency
-var remainder= difference% frequency;
+var remainder= difference % frequency;
 
-console.log("remainder:"+ " "+ remainder);
+console.log("Remainder: "+ " " + remainder);
 
 
-var minAway= frequency-remainder;
+var minsAway= frequency-remainder;
 
-console.log("min away"+ " "+minAway);
+console.log("Mins away: " + " " + minsAway);
 
-var nextArrival= moment().add(minAway, "minutes");
+var nextArrival= moment().add(minsAway, "minutes");
 
-console.log("next arrival"+" "+ nextArrival);
+console.log("Next arrival: " + " " + nextArrival);
 
 var display= moment(nextArrival).format("hh:mm");
  console.log(display); 
-console.log("*****************************************")
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 //add row
 $("#row").append('<tr>'+ '<td>'+ train+ '</td>'+ '<td>'+ dest+ '</td>'+ '<td>'+ frequency+ '</td>'+ '<td>'+ display+ '</td>'+ '<td>'+ minAway + '</td>'+'</tr>');
 
 
 
 
-})
+});
